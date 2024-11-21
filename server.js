1c4553db7e25bef61de2f9a507c2fa74bf4e96c3
tomas.novelli <tomasnovelli93@gmail.com>
2024-11-21 10:44:15 -0300

    despliegue con exito

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ import authRouter from "./src/routes/auth.route.js";
 
 const app = express()
 const PORT = ENVIROMENT.PORT
+const URL_BACK = ENVIROMENT.URL_BACK
+
 app.use(express.json({limit: '5mb'}))
 
 
@@ -15,7 +17,7 @@ app.use('/api/status', statusRouter)
 app.use('/api/auth', authRouter)
 
 app.listen(PORT, ()=>{
-    console.log(`Server Listened at port http://localhost:${PORT}`)
+    console.log(`Server Listened at port ${URL_BACK}${PORT}`)
 })
 
 
