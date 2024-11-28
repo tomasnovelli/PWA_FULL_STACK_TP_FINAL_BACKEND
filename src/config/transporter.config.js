9d586776007a3fa8cca5581b626cd774ebebf340
tomas.novelli <tomasnovelli93@gmail.com>
2024-11-27 23:03:59 -0300

    desplieuge

--- src/config/transporter.config.js
+++ src/config/transporter.config.js
@@ -3,9 +3,16 @@ import ENVIROMENT from './enviroment.config.js'
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
+    port: 465,
+    secure: true,
+    logger: true,
+    debug: true,
     auth: {
         user: ENVIROMENT.GMAIL_EMAIL,
         pass: ENVIROMENT.GMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 })
 
