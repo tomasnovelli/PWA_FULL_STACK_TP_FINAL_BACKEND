fa35a18bb7e1998dcaceacf851711e73dd82fa3e
tomas.novelli <tomasnovelli93@gmail.com>
2024-11-27 22:16:22 -0300

    nueva clave de app

--- src/config/transporter.config.js
+++ src/config/transporter.config.js
@@ -3,9 +3,6 @@ import ENVIROMENT from './enviroment.config.js'
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
     auth: {
         user: ENVIROMENT.GMAIL_EMAIL,
         pass: ENVIROMENT.GMAIL_PASS
